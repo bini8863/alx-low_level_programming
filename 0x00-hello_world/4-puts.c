ee26cac92d32afdf8432fa72d4ae1cac426deadf
bini8863 <bengaminfish@gmail.com>
2022-09-08 10:14:01 -0700

    task 4 redemption

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 /**
-* main - print the string in the put function
-* Description: using the main function
-* this program print "programming is like building a multilingual puzzel"
+* main - prints the string in puts function
 * Return: 0
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	puts("\""Programming is like building a multilingual puzzle);
+	return (0);
 }
