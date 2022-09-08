9276d6818e60c2f65bbd56da31a375a94e1eced8
bini8863 <bengaminfish@gmail.com>
2022-09-08 10:09:20 -0700

    task 5 redone

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
-* main - print the string in the printf function
+* main - prints the string in printf function
 * Return: 0
 */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}
