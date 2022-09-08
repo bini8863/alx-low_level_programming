83f09c595a78c9804743ca836e4ff5f52520868e
bini8863 <bengaminfish@gmail.com>
2022-09-08 09:12:05 -0700

    task4.7

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 *main - print the string in the put function
 *
 *Description: using the main function
-*this program print "programming is like building a multilingual puzzel
+*this program print "programming is like building a multilingual puzzel"
 *Return: 0
 */
 int main(void)
