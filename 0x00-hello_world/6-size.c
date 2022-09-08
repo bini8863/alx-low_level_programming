540c519782fac23bdca3607a68cdbbd7a025e088
bini8863 <bengaminfish@gmail.com>
2022-09-08 10:42:40 -0700

    task 6.1

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
 	float decimal;
 	printf("Size of a char: %ld byte(s)\n", sizeof(character));
 	printf("Size of an int: %ld byte(s)\n", sizeof(integer));
-	printf("Sie of a long int: %ld byte(s)\n", sizeof(longerOne));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(longerOne));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(theLongest));
 	printf("Size of a float: %ld byte(s)\n", sizeof(decimal));
 	return (0);
