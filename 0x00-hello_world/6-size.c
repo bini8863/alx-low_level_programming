df2ed75e08e7dc44a99854dc165a62edbec687e0
bini8863 <bengaminfish@gmail.com>
2022-09-08 10:36:22 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - prints various types of data
+* Return: 0
+*/
+int main(void)
+{
+	char character;
+	int integer;
+	long int longerOne;
+	long long int theLongest;
+	float decimal;
+	printf("Size of a char: %ld byte(s)\n", sizeof(character));
+	printf("Size of an int: %ld byte(s)\n", sizeof(integer));
+	printf("Sie of a long int: %ld byte(s)\n", sizeof(longerOne));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(theLongest));
+	printf("Size of a float: %ld byte(s)\n", sizeof(decimal));
+	return (0);
+}
