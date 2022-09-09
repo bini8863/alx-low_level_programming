41314cc7cbb140418da657cef23707f69d5172f7
bini8863 <bengaminfish@gmail.com>
2022-09-09 09:54:05 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - prints the lowerchase alphabate in reverse
+* Return: 0
+*/
+int main(void)
+{
+char ch;
+for (ch = 'z' ; ch <= 'a' ; ch--)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
+}
