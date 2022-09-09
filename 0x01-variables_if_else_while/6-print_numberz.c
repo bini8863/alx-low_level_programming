fb70969852de66fc001ec39487d712f2d344d555
bini8863 <bengaminfish@gmail.com>
2022-09-09 11:15:14 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - prints all numbers from "0" onward
+* Return: 0
+*/
+int main(void)
+{
+	int x;
+
+for (x = 0 ; x <= 9 ; x++)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
