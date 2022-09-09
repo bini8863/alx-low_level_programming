0fa54ae0cf204c962c4d0f7206d40f2530434b56
bini8863 <bengaminfish@gmail.com>
2022-09-09 09:12:58 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - prints the alphabet in all lower chase
+* Return: 0
+*/
+
+int main(void)
+{
+for (int x = 'a'; x <= 'z'; x++)
+{
+	putchar ("%c ", x);
+}
+return (0);
+}
+
