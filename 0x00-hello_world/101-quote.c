e3e14888dde624d0239908ea08f692998f2932d3
bini8863 <bengaminfish@gmail.com>
2022-09-10 08:01:31 -0700

    task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 
 /**
 * main - a script that prints what is in the function fprintf
-* Return: 0
+* Return: 1
 */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+putchar(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 return (1);
 }
