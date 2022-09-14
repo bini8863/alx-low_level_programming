051c2015a8e28688836b7ce42e7e459669f3dc66
bini8863 <bengaminfish@gmail.com>
2022-09-13 22:20:07 -0700

    task 8 initial commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,5 +7,27 @@
 
 void jack_bauer(void)
 {
+	int hours = 0;
+	int minutes = 0;
+	int hours_reminder;
+	int mins_reminder;
+
+	while (hours < 23)
+	{
+	while (minutes < 59)
+	{
+	mins_reminder = minutes % 10;
+	hours_reminder = hours % 10;
+	_putchar(hours / 10 + '0');
+	_putchar(hours_reminder + '0');
+	_putchar(':');
+	_putchar(minutes / 10 + '0');
+	_putchar(mins_reminder + '0');
+	_minutes++;
+	_putchar('\n');
+	}
+	hours++;
+	minutes = 0;
+	}
 
 }
