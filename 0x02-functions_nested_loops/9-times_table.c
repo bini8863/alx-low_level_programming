af54e7dafbbe32839c165b494c2ba890a3233abd
bini8863 <bengaminfish@gmail.com>
2022-09-14 10:01:31 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,12 +3,11 @@
 /**
 * times_table - prints the 9 times table, starting with 0.
 */
-
 void times_table(void)
 {
 	int num, multi, prod;
 
-	for (num = 0; num <= 0; num++)
+	for (num = 0; num <= 9; num++)
 	{
 	_putchar('0');
 	for (multi = 1; multi <= 9; multi++)
