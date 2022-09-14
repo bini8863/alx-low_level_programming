a4f5269f963623b3fc20c83ad566934a9db68a09
bini8863 <bengaminfish@gmail.com>
2022-09-14 10:09:28 -0700

    task 10 initial commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* add - adds the arguments
+* @i: is argument number one
+* @j: is the second argument
+* Return: result of add
+*/
+
+int add(int i, int j)
+{
+	return (i + j);
+}
