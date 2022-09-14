de43abc75580ecb52eae65da59f4453679396b2a
bini8863 <bengaminfish@gmail.com>
2022-09-14 11:22:08 -0700

    task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_times_table - prints n times table
+* @n: the argument
+*/
+
+void print_times_table(int n)
+{
+	if (n > 15 || n < 0)
+	{
+	_putchar();
+	}
+	else
+		_putchar(n);
+}
