30adf13e1faa701e30087721e1653879c78dd8bb
bini8863 <bengaminfish@gmail.com>
2022-09-13 19:33:40 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,23 +3,25 @@
 /**
 * print_sign - returns according to the value
 * Return: 0
+* @n - is the argument
 */
 
 int print_sign(int n)
 {
-if (n > 0)
-	{
+int n;
+	if (n > 0)
+{
 	_putchar('+');
 	return (1);
-	}
-else if (n = 0)
-	{
-	_putchar(0);
-	return (0);
-	}
-else
+}
+	else if (n < 0)
 	{
 	_putchar('-');
 	return (-1);
+	}
+	else
+	{
+	_putchar('0');
+	return (0);
 	}
 }
