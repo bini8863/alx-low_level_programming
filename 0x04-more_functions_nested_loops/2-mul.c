59a9b5421ba9738d491dbff146988b57ff340d1d
bini8863 <bengaminfish@gmail.com>
2022-09-15 08:50:09 -0700

    task 2 initial commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* mul - multiplies 2 intigers
+* @a: the first argument
+* @b: the second argument
+* Return: 0
+*/
+
+int mul(int a, int b)
+{
+	printf("%d", a * b);
+	return (0);
+}
