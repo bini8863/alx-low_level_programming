5e4aa70bd19691f280d9e99e79d684fd3df65133
bini8863 <bengaminfish@gmail.com>
2022-09-15 07:33:04 -0700

    fixed betty error

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-* print_remaining_days - takes a date and prints how many days are left in the year
+* print_remaining_days - prints how many days are left in a year
 * @month: month in number format
 * @day: day of month
 * @year: year
-* Return: void 
+* Return: void
 */
 
 void print_remaining_days(int month, int day, int year)
