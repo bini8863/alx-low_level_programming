358cbbc140ea38219775dd9fce3ecef49f6b0ebe
bini8863 <bengaminfish@gmail.com>
2022-09-16 01:03:23 -0700

    fizz bizz 2

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,31 +2,31 @@
 
 /**
 * main - fizz_bizz test
-* @n1: parameter
+* @n: parameter
 * Return: 0
 */
 
 int main(void)
 {
-	int n1 = 1;
+	int n = 1;
 
-	while (n1 < 100; n++)
+	while (n++ < 100)
 	{
-		if ((n1 % 3 == 0) && (n1 % 5 == 0))
+		if ((n % 3 == 0) && (n % 5 == 0))
 		{
 			printf("FizzBuzz ");
 		}
-		else if (n1 % 3 == 0)
+		else if (n % 3 == 0)
 		{
 			printf("Fizz");
 		}
-		else if (n1 % 5 == 0)
+		else if (n % 5 == 0)
 		{
 			printf("Buzz ");
 		}
 		else
 		{
-			printf("%d ", n1);
+			printf("%d ", n);
 		}
 	}
 	return (0);
