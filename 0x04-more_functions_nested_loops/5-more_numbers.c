50dff375e750c3d5aa453dcf221eb9efd7a6a6f7
bini8863 <bengaminfish@gmail.com>
2022-09-15 22:21:42 -0700

    task 4 initial commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* more_numbers - prints numbers from 0 to 14 ten times followed by a new line.
+*/
+
+void more_numbers(void)
+{
+	int n,i;
+	for (i = 0; i < 10; i++)
+	{
+	for (n = 48; n < 62; n++_)
+	{
+		putchar(n);
+	}
+	i++;
+	putchar(10);
+	}
+}
