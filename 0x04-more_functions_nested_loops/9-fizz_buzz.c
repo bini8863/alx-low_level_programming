0b886084fd2b525769e8fd82c2c665d6492eddd1
bini8863 <bengaminfish@gmail.com>
2022-09-16 01:19:24 -0700

    fizz bizz with for loop

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,21 +8,21 @@
 
 int main(void)
 {
-	int n = 1;
+	int n;
 
-	while (n++ < 100)
+	for (n = 1; n <= 100; n++)
 	{
 		if ((n % 3 == 0) && (n % 5 == 0))
 		{
-			printf(" FizzBuzz ");
+			printf("FizzBuzz ");
 		}
 		else if (n % 3 == 0)
 		{
-			printf(" Fizz");
+			printf("Fizz ");
 		}
 		else if (n % 5 == 0)
 		{
-			printf(" Buzz ");
+			printf("Buzz ");
 		}
 		else
 		{
