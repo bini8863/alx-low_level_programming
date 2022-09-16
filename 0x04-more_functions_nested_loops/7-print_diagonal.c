dece0f7efac4977b11320625d5b5eb1b3f51bfea
bini8863 <bengaminfish@gmail.com>
2022-09-15 23:54:52 -0700

    task 7 initial commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_diagonal - prints diagonal lines
+* @n: the number of times the diagonal line prints
+* print_diagonal - prints diagonal lines.
+* Return: nothing
+*/
+
+void print_diagonal(int n)
+{
+	while (n-- > 0)
+	{
+		putchar('\');
+	}
+	putchar('\n');
+}
