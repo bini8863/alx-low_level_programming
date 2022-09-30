8393c2ab10bb6db9c57f0d432370269e034370a8
bini8863 <bengaminfish@gmail.com>
2022-09-30 08:51:31 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - prints the name of the program
+* @argc: counter argument
+* @argv: vector argument
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+printf("%s", argv[0]);
+return (0);
+}
