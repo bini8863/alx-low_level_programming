9c6d437aba6f6f5757e45d922a9cfd1efc8c8710
bini8863 <bengaminfish@gmail.com>
2022-10-06 12:00:57 -0700

    task 100.1

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,7 +13,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *p;
 	unsigned int i, max = new_size;
-
+	char *oldp = ptr;
 	if (ptr == NULL)
 	{
 		p = malloc(new_size);
@@ -32,7 +32,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size > old_size)
 		max = old_size;
 	for (i = 0; i < max; i++)
-		p[i] = old_size[i];
+		p[i] = oldp[i];
 	free(ptr);
 	return (p);
 }
