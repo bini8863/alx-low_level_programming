8a3165dc0b33d6c4bd61b81df99d37703e7bb753
bini8863 <bengaminfish@gmail.com>
2022-10-11 08:56:35 -0700

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+* main - prints the name of the excutable
+* @argc: argument count
+* @argv: number of argument
+* Return: 0
+*/
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
