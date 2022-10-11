9a3b803335809da04daa71b19ecfab3956da1e72
bini8863 <bengaminfish@gmail.com>
2022-10-11 08:17:47 -0700

    task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
 * init_dog - creates a variable of type struct dog
@@ -10,10 +11,9 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
-	{
+	if (d == 0)
+		return (0);
 		d->name = name;
 		d->owner = owner;
 		d->age = age;
-	}
 }
