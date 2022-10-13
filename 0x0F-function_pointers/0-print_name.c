e1cb7b1b079467d957fd05626b9b56aed9b98916
bini8863 <bengaminfish@gmail.com>
2022-10-12 19:45:37 -0700

    task 1 and 2

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+/**
+* print_name - prints name.
+* @name: pointer to name.
+* @f: functuion pointer
+*
+* Return: void
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
