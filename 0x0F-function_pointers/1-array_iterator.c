4ef3dfdf04614cf05c5ac52209dff4c852b12c5a
bini8863 <bengaminfish@gmail.com>
2022-10-12 19:58:28 -0700

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* array_iterator - funcion that executes a given parameter
+* @array: array of elements
+* @size: size of array
+* @action: function pointer
+*
+* Return: void
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array && size > 0 && action)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
