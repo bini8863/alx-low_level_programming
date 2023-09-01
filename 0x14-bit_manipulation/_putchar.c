c18b2f8f42466f6dddaa7a0982a51176a496ddb1
bini8863 <bengaminfish@gmail.com>
2023-09-01 12:34:17 -0700

    all files are now betty complient

--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 
 /**
- * _putchar: prints c to std out
+ * _putchar - prints c to stdout
  * @c: cahr to print
  * Return: 1 on sucess or -1
  */
 
 int _putchar(char c)
 {
-	return(write(1, &c, 1));
+	return (write(1, &c, 1));
 }
 
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,9 +9,11 @@
 * main - header file
 * void prototype (void)
 * int prototype (int)
+* @b: the char to be converted
+* Return: Always zero.
 */
 
-unsigned int binary_to_uint(const char * b);
+unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
